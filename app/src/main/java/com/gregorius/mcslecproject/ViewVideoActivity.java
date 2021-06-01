5136faefd96ced96d6495dcbabc0be8f4fbbe714
ivannsu <ivansu.webmail@gmail.com>
2021-06-01 18:44:23 +0700

    finish view music video

--- app/src/main/java/com/gregorius/mcslecproject/ViewVideoActivity.java
+++ app/src/main/java/com/gregorius/mcslecproject/ViewVideoActivity.java
@@ -1,14 +1,80 @@
 package com.gregorius.mcslecproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class ViewVideoActivity extends AppCompatActivity {
+
+    RecyclerView rvMedia;
+    EditText etKeyword;
+    Button btnSearch;
+
+    MediaAdapter mediaAdapter;
+    List<MediaResponse> listMedia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_video);
+
+        rvMedia = findViewById(R.id.rvMedia);
+        etKeyword = findViewById(R.id.etKeyword);
+        btnSearch = findViewById(R.id.btnSearch);
+
+        LinearLayoutManager llManager = new LinearLayoutManager(this);
+        rvMedia.setLayoutManager(llManager);
+
+        mediaAdapter = new MediaAdapter(this);
+        rvMedia.setAdapter(mediaAdapter);
+
+        // Loading Data for the first time with the term 'love'
+        search("love");
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String term = etKeyword.getText().toString();
+                search(term);
+            }
+        });
+    }
+
+    private void search(String term) {
+        Retrofit retrofit = APIClient.getRetrofit();
+        iTunesService service = retrofit.create(iTunesService.class);
+
+        Call<MediaListResponse> call = service.searchMedia("musicVideo", term);
+        call.enqueue(new Callback<MediaListResponse>() {
+            @Override
+            public void onResponse(Call<MediaListResponse> call, Response<MediaListResponse> response) {
+                if (response.isSuccessful()) {
+                    MediaListResponse mediaListResponse = response.body();
+                    listMedia = mediaListResponse.getResults();
+
+                    mediaAdapter.setListMedia((ArrayList<MediaResponse>) listMedia);
+
+                    // Log.i("MY_TAG", "onResponse: " + listMusic.get(0).getTrackName());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MediaListResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
